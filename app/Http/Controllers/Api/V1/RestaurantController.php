145b95b5220524d7b1aeae85436cbcb7896116bd
FrostElAkuma <Vilgaks2002@yahoo.com>
2023-02-23 13:44:49 +0400

    Tracking and naming uploaded file

--- app/Http/Controllers/Api/V1/RestaurantController.php
+++ app/Http/Controllers/Api/V1/RestaurantController.php
@@ -40,14 +40,14 @@ class RestaurantController extends Controller
      
        if ($request->hasFile('image')) {
                 //generating a unique id for the image and savinf it as a png
-               //$imageName = \Carbon\Carbon::now()->toDateString() . "-" . uniqid() . "." . "png";
+               $imageName = \Carbon\Carbon::now()->toDateString() . "-" . uniqid() . "." . "png";
                //Checking if we have a public directory in our server. If not we create it
                /*if (!Storage::disk('public')->exists($dir)) {
                    Storage::disk('public')->makeDirectory($dir); //public/test
                }*/
                //Storage::disk('public')->put('images/'.$imageName, file_get_contents($image));
             //Below line took me about 2 hours to get right
-            $path = $request->image->store('images', 'admin');
+            $path = $request->image->storeAs('images', $imageName, 'admin');
        }/*else{
             return response()->json(['message' => trans('/storage/test/'.'def.png')], 200);
        } */
@@ -63,7 +63,7 @@ class RestaurantController extends Controller
        //myServer.come/storage/test/imagename.png
        //The above is for the testing video, but in reality I am storing my images under uploads/images. So myseverver.com/uplouds/images/myImage.png
        //Please note i should have the full image path below. Currently I dont but ned to update in future
-       return response()->json(['message' => trans('/storage/test/')], 200);
+       return response()->json(['message' => trans('images/'.$imageName)], 200);
    }
 
 }
